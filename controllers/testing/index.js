7398330a75914f2359b06b1edc28d8b42a3a2e1d
Rionase <nicholasssputra@gmail.com>
2024-10-27 17:24:43 +0700

    FEAT : Add Progress rate in insert-seats API

--- controllers/testing/index.js
+++ controllers/testing/index.js
@@ -2,11 +2,15 @@ const flightUsecase = require("../../usecases/flight/index");
 const seatUsecase = require("../../usecases/seat/index");
 
 const { v4: uuidv4 } = require("uuid");
+const { recomendation } = require("../../helpers/recomendation")
 
 exports.insertSeat = async (req,res,next) => {
 
     // GET FLIGHTS LIMIT NEED TO BE DISABLED TO GET ALL FLIGHTS
     const allFlights = await flightUsecase.getFlights();
+
+    let total = allFlights.length
+    let count = 0
 
     for ( let i = 0 ; i < allFlights.length ; i++ ) {
         data = allFlights[i]
@@ -21,48 +25,54 @@ exports.insertSeat = async (req,res,next) => {
         if ( data["StartAirport"]["country"] == "Indonesia" && data["EndAirport"]["country"] == "Indonesia" ) {
 
             let tmp = await seatUsecase.getSeatbyFlight(flightId)
-            if ( tmp ) continue
+            if ( tmp ) {
+                continue
+            } else {
+                // ! RUNNING THE CODE AGAIN MAY MAKE THE DATA DUPLICATE
 
-            // ! RUNNING THE CODE AGAIN MAY MAKE THE DATA DUPLICATE
+                // let airlineClass = "ECONOMY";
+                // for (let iEconomy = 1 ; iEconomy <= economy ; iEconomy++ ) {
+                //     let id = uuidv4();
+                //     let seatNumber = iEconomy;
+                //     await seatUsecase.createSeat({
+                //         id,
+                //         flightId,   
+                //         seatNumber,
+                //         airlineClass,
+                //         isAvailable
+                //     })
+                // }
 
-            // let airlineClass = "ECONOMY";
-            // for (let iEconomy = 1 ; iEconomy <= economy ; iEconomy++ ) {
-            //     let id = uuidv4();
-            //     let seatNumber = iEconomy;
-            //     await seatUsecase.createSeat({
-            //         id,
-            //         flightId,
-            //         seatNumber,
-            //         airlineClass,
-            //         isAvailable
-            //     })
-            // }
+                // airlineClass = "BUSINESS"
+                // for (let iBussiness = 1 ; iBussiness <= business ; iBussiness++ ) {
+                //     let id = uuidv4();
+                //     let seatNumber = iBussiness;
+                //     await seatUsecase.createSeat({
+                //         id,
+                //         flightId,
+                //         seatNumber,
+                //         airlineClass,
+                //         isAvailable
+                //     })
+                // }
 
-            // airlineClass = "BUSINESS"
-            // for (let iBussiness = 1 ; iBussiness <= business ; iBussiness++ ) {
-            //     let id = uuidv4();
-            //     let seatNumber = iBussiness;
-            //     await seatUsecase.createSeat({
-            //         id,
-            //         flightId,
-            //         seatNumber,
-            //         airlineClass,
-            //         isAvailable
-            //     })
-            // }
+                // airlineClass = "FIRST_CLASS"
+                // for (let iFirst = 1 ; iFirst <= firstClass ; iFirst++ ) {
+                //     let id = uuidv4();
+                //     let seatNumber = iFirst;
+                //     await seatUsecase.createSeat({
+                //         id,
+                //         flightId,
+                //         seatNumber,
+                //         airlineClass,
+                //         isAvailable
+                //     })
+                // }
 
-            // airlineClass = "FIRST_CLASS"
-            // for (let iFirst = 1 ; iFirst <= firstClass ; iFirst++ ) {
-            //     let id = uuidv4();
-            //     let seatNumber = iFirst;
-            //     await seatUsecase.createSeat({
-            //         id,
-            //         flightId,
-            //         seatNumber,
-            //         airlineClass,
-            //         isAvailable
-            //     })
-            // }
+                count += 1
+                console.log(`============== PROGRESS NUMBER ${count} / ${total} ====================`)
+            }
+
         }
     }
 
